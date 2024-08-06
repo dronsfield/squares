3e61abdedddd1fc19d0366d1ef1c68ed917b98c0
Josh <josh.dronsfield@gmail.com>
2024-08-07 00:36:53 +0100

    lol

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ current_epoch="$start_epoch"
 while [ "$current_epoch" -le "$end_epoch" ]; do
     current_date=$(date -j -f "%s" "$current_epoch" "+%Y-%m-%d")
     echo "$current_date" >> lol.txt
-    git add .
+    git add lol.txt
     git commit --date=$(date -j -f "%s" "$current_epoch" +"%Y-%m-%dT%H:%M:%S") -m "$current_date"
     current_epoch=$(($current_epoch + 86400))
 done
